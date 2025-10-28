a4909134be8435a7cf9fb82db646b39088296474
Suneilkumar <suneilkumar@gmail.com>
2025-10-28 15:11:29 +0530

    geatr

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App";
-import "./index.css"; // Tailwind
 
 const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+const clerkFrontendAPI = process.env.REACT_APP_CLERK_FRONTEND_API;
+
+if (!clerkPubKey) {
+  console.error("Missing Clerk publishable key.");
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={clerkPubKey}>
+    <ClerkProvider
+      publishableKey={clerkPubKey}
+      frontendApi={clerkFrontendAPI}
+    >
       <App />
     </ClerkProvider>
   </React.StrictMode>
